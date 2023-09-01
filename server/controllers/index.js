25b488ca7550dcbc60c103ab33edae47a902474a
Josh <wjang717@gmail.com>
2023-08-31 20:51:36 -0700

    code clean up

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,46 +3,52 @@ const baseURL ='http://54.183.201.13:3000';
 // const baseURL ='http://127.0.0.1:3000';
 
 module.exports = {
-  getIngredients: (req, res) => {
-    axios.get(`${baseURL}/api/ingredients`)
-    .then(({data}) => res.send(data));
-  },
-  getTypes: (req, res) => {
-    axios.get(`${baseURL}/api/types`)
-    .then(({data}) => res.send(data));
-  },
+  // GET ALL RECIPES WITH TYPE ID
   getMenuByType: (req, res) => {
     axios.get(`${baseURL}/api/recipes/?id=${req.query.id}&sort=${req.query.sort}`)
     .then(({data}) => res.send(data));
   },
-  rateAMenu: (req, res) => {
-    axios.put(`${baseURL}/api/rating/?id=${req.query.id}&prev=${req.query.prev}&rate=${req.query.new}`)
-    .then(({data}) => res.send(data));
-  },
-  getRecipe: (req, res) => {
-    axios.get(`${baseURL}/api/recipe/?id=${req.query.id}`)
-    .then(({data}) => res.send(data));
-  },
-  addImg: (req, res) => {
-    axios.post(`${baseURL}/api/image`, req.body)
-    .then(({data}) => res.send(data));
-  },
-  search: (req, res) => {
-    axios.get(`${baseURL}/api/recipes/search/?input=${req.query.input}`)
-    .then(({data}) => res.send(data));
-  },
-  getTypesAla: (req, res) => {
-    axios.get(`${baseURL}/api/types/ala`)
-    .then(({data}) => res.send(data));
-  },
+  // GET ALL RECIPES WITH AN ARRAY OF RECIPE IDs
   favorites: (req, res) => {
     axios.get(`${baseURL}/api/favorites/?fav=${req.query.fav}`)
     .then(({data}) => res.send(data));
   },
+  // UPDATE FAVORITE COUNT FOR THE MENU WITH RECIPE ID
+  updateFavorite: (req, res) => {
+    axios.put(`${baseURL}/api/favorite/?id=${req.query.id}&edit=${req.query.edit}`)
+    .then(({data}) => res.send(data));
+  },
+  // GET ALL RECIPES THAT CONTAINS INPUT STRING AS SUBSTRING
+  search: (req, res) => {
+    axios.get(`${baseURL}/api/recipes/search/?input=${req.query.input}`)
+    .then(({data}) => res.send(data));
+  },
+  // GET A SINGLE MENU WITH RECIPE ID
+  getRecipe: (req, res) => {
+    axios.get(`${baseURL}/api/recipe/?id=${req.query.id}`)
+    .then(({data}) => res.send(data));
+  },
+  // GET ALL INGREDIENTS IN DB
+  getIngredients: (req, res) => {
+    axios.get(`${baseURL}/api/ingredients`)
+    .then(({data}) => res.send(data));
+  },
+  // GET ALL TYPES IN DB
+  getTypes: (req, res) => {
+    axios.get(`${baseURL}/api/types`)
+    .then(({data}) => res.send(data));
+  },
+  // GET ALL TYPES IN DB SO THAT ALACARTES COME FIRST IN THE LIST
+  getTypesAla: (req, res) => {
+    axios.get(`${baseURL}/api/types/ala`)
+    .then(({data}) => res.send(data));
+  },
+  // CREATE A RATING AND REVIEW FOR A MENU
   ratings: (req, res) => {
     axios.post(`${baseURL}/api/ratings`, req.body)
     .then(({data}) => res.send(data));
   },
+  // UPDATE A RATING AND REVIEW FOR A MENU
   updateRatings: (req, res) => {
     const {id, rating, review} = req.query;
     axios.put(`${baseURL}/api/ratings/?id=${id}&rating=${rating}&review=${review}`)
@@ -50,25 +56,30 @@ module.exports = {
       res.send(data);
     })
   },
+  // GET A REVIEW WITH REVIEW ID
   getReviews: (req, res) => {
     axios.get(`${baseURL}/api/ratings/?id=${req.query.id}`)
     .then(({data}) => res.send(data));
   },
+  // UPDATE RECIPE WITH IMG URL
+  addImg: (req, res) => {
+    axios.post(`${baseURL}/api/image`, req.body)
+    .then(({data}) => res.send(data));
+  },
+  // GET RATINGS WITH RECIPE ID
+  getReview: (req, res) => {
+    axios.get(`${baseURL}/api/getReview/?id=${req.query.id}`)
+    .then(({data}) => res.send(data));
+  },
+  // UPDATE HELPFUL COUNT FOR REVIEW
   updateHelpful: (req, res) => {
     const {id, update} = req.query;
     axios.put(`${baseURL}/api/helpful/?id=${id}&update=${update}`)
     .then(({data}) => res.send(data));
   },
+  // UPDATE REPORT STATUS OF REVIEW
   updateReport: (req, res) => {
     axios.put(`${baseURL}/api/review/report/?id=${req.query.id}`)
-    .then(({data}) => res.send(data));
-  },
-  updateFavorite: (req, res) => {
-    axios.put(`${baseURL}/api/favorite/?id=${req.query.id}&edit=${req.query.edit}`)
-    .then(({data}) => res.send(data));
-  },
-  getReview: (req, res) => {
-    axios.get(`${baseURL}/api/getReview/?id=${req.query.id}`)
     .then(({data}) => res.send(data));
   }
 }
--- server/routes.js
+++ server/routes.js
@@ -1,40 +1,50 @@
 const router = require('express').Router();
 const controllers = require('./controllers');
 
-router.get('/hello', (req, res) => {
-  res.send('hello');
-});
-
-router.get('/ingredients', controllers.getIngredients);
-
-router.get('/types', controllers.getTypes);
-
-router.get('/types/ala', controllers.getTypesAla);
-
+// GET ALL RECIPES WITH TYPE ID
 router.get('/recipesWithType', controllers.getMenuByType);
 
-router.put(`/rating`, controllers.rateAMenu);
-
-router.get('/recipe', controllers.getRecipe);
-
-router.post('/img', controllers.addImg);
-
-router.get('/search', controllers.search);
-
+// GET ALL RECIPES WITH AN ARRAY OF RECIPE IDs
 router.get('/favorites', controllers.favorites);
 
-router.post('/ratings', controllers.ratings);
-
-router.put('/ratings', controllers.updateRatings);
-
-router.get('/ratings', controllers.getReviews);
-
-router.put('/helpful', controllers.updateHelpful);
-
-router.put('/review/report', controllers.updateReport);
-
+// UPDATE FAVORITE COUNT FOR THE MENU WITH RECIPE ID
 router.put('/favorite', controllers.updateFavorite);
 
+// GET ALL RECIPES THAT CONTAINS INPUT STRING AS SUBSTRING
+router.get('/search', controllers.search);
+
+// GET A SINGLE MENU WITH RECIPE ID
+router.get('/recipe', controllers.getRecipe);
+
+// GET ALL INGREDIENTS IN DB
+router.get('/ingredients', controllers.getIngredients);
+
+// GET ALL TYPES IN DB
+router.get('/types', controllers.getTypes);
+
+// GET ALL TYPES IN DB SO THAT ALACARTES COME FIRST IN THE LIST
+router.get('/types/ala', controllers.getTypesAla);
+
+// CREATE A RATING AND REVIEW FOR A MENU
+router.post('/ratings', controllers.ratings);
+
+// UPDATE A RATING AND REVIEW FOR A MENU
+router.put('/ratings', controllers.updateRatings);
+
+// GET A REVIEW WITH REVIEW ID
 router.get('/getReview', controllers.getReview);
+
+// UPDATE RECIPE WITH IMG URL
+router.post('/img', controllers.addImg);
+
+// GET RATINGS WITH RECIPE ID
+router.get('/ratings', controllers.getReviews);
+
+// UPDATE HELPFUL COUNT FOR REVIEW
+router.put('/helpful', controllers.updateHelpful);
+
+// UPDATE REPORT STATUS OF REVIEW
+router.put('/review/report', controllers.updateReport);
+
 
 module.exports = router;
